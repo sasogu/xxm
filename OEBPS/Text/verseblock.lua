03ff903c353abd527eb7d2ddea3fc899bec0f096
Tu Nombre <sasogu@gmail.com>
2025-04-11 00:08:21 +0200

    conseguido salto de página

--- OEBPS/Text/verseblock.lua
+++ OEBPS/Text/verseblock.lua
@@ -12,8 +12,11 @@ function Div(el)
   
   function Header(el)
     if el.level == 1 and FORMAT:match('latex') then
-        -- Centrar encabezados <h1>
-        
-        return pandoc.RawBlock('latex', '\\begin{center}\\textbf{' .. pandoc.utils.stringify(el.content) .. '}\\end{center}')
+      -- Salto de página + centrar encabezados h1
+      return {
+        pandoc.RawBlock('latex', '\\clearpage'),
+        pandoc.RawBlock('latex', '\\begin{center}\\Huge\\textbf{' .. pandoc.utils.stringify(el.content) .. '}\\end{center}')
+      }
     end
-end
+  end
+  
