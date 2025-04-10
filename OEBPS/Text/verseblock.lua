9188a2e927cc56cc80a5ecea466a7b5874224ad7
Tu Nombre <sasogu@gmail.com>
2025-04-11 00:05:31 +0200

    centrado falta salto de página

--- OEBPS/Text/verseblock.lua
+++ OEBPS/Text/verseblock.lua
@@ -1,27 +1,19 @@
 function Div(el)
     if el.classes:includes('verse') then
-        if FORMAT:match('latex') then
-            -- Procesar el bloque con clase 'verse'
-            return {
-                pandoc.RawBlock('latex', '\\begin{verseblock}'),
-                el,
-                pandoc.RawBlock('latex', '\\end{verseblock}')
-            }
-        end
-    end
-end
-
-function Header(el)
-    if el.level == 1 and FORMAT:match('latex') then
-        -- Añadir un salto de página antes de cada <h1> y centrarlo
-        local content = pandoc.utils.stringify(el.content)
+      if FORMAT:match('latex') then
         return {
-            pandoc.RawBlock('latex', '\\newpage'),
-            pandoc.RawBlock('latex', '\\begin{center}\\textbf{' .. content .. '}\\end{center}')
+          pandoc.RawBlock('latex', '\\begin{center}'),
+          el,
+          pandoc.RawBlock('latex', '\\end{center}')
         }
+      end
+    end
+  end
+  
+  function Header(el)
+    if el.level == 1 and FORMAT:match('latex') then
+        -- Centrar encabezados <h1>
+        
+        return pandoc.RawBlock('latex', '\\begin{center}\\textbf{' .. pandoc.utils.stringify(el.content) .. '}\\end{center}')
     end
 end
-
-\newenvironment{verseblock}
-  {\begin{center}\begin{minipage}{0.8\textwidth}}
-  {\end{minipage}\end{center}}
