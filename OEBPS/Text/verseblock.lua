e27a043364cb7a907f7dba865b1c7df481de79a6
Tu Nombre <sasogu@gmail.com>
2025-04-11 14:40:11 +0200

    cometario

--- OEBPS/Text/verseblock.lua
+++ OEBPS/Text/verseblock.lua
@@ -1,5 +1,5 @@
 function Div(el)
-    if el.classes:includes('verse') then
+    if el.classes:includes('verse2') then
       if FORMAT:match('latex') then
         return {
           pandoc.RawBlock('latex', '\\begin{center}'),
@@ -9,6 +9,24 @@ function Div(el)
       end
     end
   end
+
+  function Div(el)
+    if el.classes:includes('verse') then
+        if FORMAT:match('latex') then
+            local content = pandoc.write(pandoc.Pandoc(el.content), "latex")
+            return {
+                pandoc.RawBlock('latex', '\\clearpage'),
+                pandoc.RawBlock('latex', '\\vspace*{\\fill}'),
+                pandoc.RawBlock('latex', '\\noindent\\begin{minipage}{\\textwidth}\\centering'),
+                pandoc.RawBlock('latex', content),
+                pandoc.RawBlock('latex', '\\end{minipage}'),
+                pandoc.RawBlock('latex', '\\vspace*{\\fill}'),
+                pandoc.RawBlock('latex', '\\clearpage')
+            }
+        end
+    end
+end
+
   
   function Header(el)
     if el.level == 1 and FORMAT:match('latex') then
@@ -19,5 +37,4 @@ function Div(el)
       }
     end
   end
-  
-  
+
